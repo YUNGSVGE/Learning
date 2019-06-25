79d86384601053d365d7241d80c7fe5d157333c4
YUNG SVGE <svgeyung@gmail.com>
2019-06-25 05:04:01 -0500

    char.py

---
+++ Char.py
@@ -0,0 +1,21 @@
+### assign a to char z 
+a = 'z'
+
+
+### 1 return to the start 
+#b = "\r"
+
+
+### 2 Tab
+#b = "\t"
+
+### 3 Qoutes ( Single Qoute , Double qoute)
+#b = "\'"
+#b = '\"'
+
+### 4 Backslash
+#b = "\\"
+
+### z + return + a again = to only 1 Z
+c =  a + b + a
+print(c)
